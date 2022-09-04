79e298c7ce36bccc5f3913bcc8ee94a1f95138c0
Davi Pinheiro <davipinheiro1993@gmail.com>
2022-09-04 11:07:22 -0300

    Update Calculator

---
+++ layouts/default.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="principal h-screen w-full bg-fixed bg-no-repeat ">
+    <div class="container mx-auto w-full h-screen sm:h-4/5 sm:w-4/5">
+      <Nuxt />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+.principal {
+  background-image: url('../static/gymwp.jpg');
+  background-size: 100%;
+  
+  
+}
+
+@media (max-width: 600px) {
+      .principal {
+        background-position: -630px -0px;
+        background-size: cover;
+      }
+}
+
+.container {
+    border: 1px solid green;
+
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class=" w-full h-full flex align-between justify-center">
+    <div class="grid grid-cols-1 gap-1">
+      <div class="grid grid-rows-1 ">
+        <div class="font-sans text-white p-4 text-3xl sm:text-5xl text-center" >Descubra sua taxa metabólica basal!</div>
+      </div>
+      <div class="grid grid-rows-1 text-white h-11 px-4 sm:flex sm:justify-center" >
+        <nuxt-link to="/sexo">
+          <button class="w-full sm:w-min text-white transition-colors duration-150 bg-green-500 sm:rounded-lg focus:shadow-outline hover:bg-gray-800 px-4 py-2 shadow  " >Começar</button>
+        </nuxt-link>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Index',
+  layout: 'default',
+}
+</script>
+
+<style>
+
+</style>
