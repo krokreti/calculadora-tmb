cb1fe7df50acb22967d1f85a4f38590fad4252ec
davi <davipinheiro1993@gmail.com>
2022-09-05 15:21:00 -0300

    New Page Design

---
+++ components/Ui/InputForm.vue
@@ -0,0 +1,61 @@
+<template>
+    <div class="w-full sm:w-2/5 border border-gray-500 rounded-lg p-4">
+        <label class="flex my-2" for="formInput">{{ label }}</label>
+        <input
+        :placeholder="placeholder" 
+        :value="value"
+        v-on="inputListeners"
+        class=" 
+        bg-gradient-to-br from-black to-gray-700
+        w-full h-10 px-3 text-base 
+        placeholder-gray-400 border 
+        border-blue-500 rounded-lg 
+        text-white
+        focus:shadow-outline" type="text" id="formInput"/>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'InputForm',
+    props: {
+        value: {
+        type: [String, Number],
+        default: undefined,
+        },
+        placeholder: {
+        type: String,
+        default: undefined,
+        },
+        label: null,
+    },
+    
+    data() {
+    return {
+      localValue: this.value,
+    };
+  },
+
+  watch: {
+    value(value) {
+      this.localValue = value;
+    },
+  },
+
+  computed: {
+    inputListeners() {
+      const vm = this;
+      return {
+        ...this.$listeners,
+        input(event) {
+          vm.$emit('input', event.target.value);
+        },
+      };
+    },
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
--- pages/peso/index.vue
+++ pages/peso/index.vue
@@ -1,16 +1,39 @@
 <template>
-  <div class="text-white text-center text-2xl block bg-gradient-to-br from-black to-gray-700 h-screen">
+  <div class="text-white text-center text-2xl block bg-gradient-to-br from-black to-gray-700 h-screen block">
     Digite seu peso em Kg:
-    <div class="w-full sm:w-2/5 border border-gray-500 rounded-lg p-4">
-        <label class="flex my-2" for="formGridCode_card">Peso</label>
-        <input class=" w-full h-10 px-3 text-base placeholder-gray-600 border border-blue-500 rounded-lg focus:shadow-outline" type="text" id="formGridCode_card"/>
+    <InputForm :placeholder="'Peso'" :value="peso" :label="'Peso'" v-model="peso"/>
+    <div class="grid grid-cols-2 gap-1 h-32" style="border:1px solid red">
+      <div>
+        <button class=" h-full w-4/5 transition-colors duration-150 text-blue-700 border border-blue-500 rounded-lg focus:shadow-outline hover:bg-blue-600 hover:border-blue-400 hover:text-white "> 
+          <span class="icon">
+            <font-awesome-icon :icon="['fa', 'angles-left']" class="text-6xl"></font-awesome-icon>
+          </span>
+        </button>
+      </div>
+      <div>
+        <button class=" h-full w-4/5 transition-colors duration-150 text-blue-700 border border-blue-500 rounded-lg focus:shadow-outline hover:bg-blue-600 hover:border-blue-400 hover:text-white "> 
+          <span class="icon">
+            <font-awesome-icon :icon="['fa', 'angles-right']" class="text-6xl"></font-awesome-icon>
+          </span>
+        </button>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-export default {
+import InputForm from '../../components/Ui/InputForm.vue'
 
+export default {
+  name: 'peso',
+  data() {
+    return {
+      peso: null,
+    }
+  },
+  components: {
+    InputForm,
+  },
 }
 </script>
 
